48e89568ea829e46b24c6d1fcb8a694eb0f3fc27
juan.carlos <jcarlos.ad7@gmail.com>
2023-07-18 19:05:37 -0500

    Ficheros

---
+++ src/Ejercicios/LeerFichero.java
@@ -0,0 +1,19 @@
+package Ejercicios;
+import java.io.FileInputStream; //
+public class LeerFichero {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fichero=new FileInputStream("D:\\UTP 2023\\Taller de Programacion\\fichero1.txt");
+            System.out.println("# Datos del archivo #");
+            int ch=fichero.read();
+            while(ch!=-1){
+                System.out.println((char) ch);
+                ch=fichero.read();
+            }
+            fichero.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Sigue funcionando");
+    }
+}
---
+++ src/Ejercicios/LeerFicheroPalabra.java
@@ -0,0 +1,26 @@
+package Ejercicios;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerFicheroPalabra {
+    public static void main(String[] args) {
+        try{
+            FileReader fichero=new FileReader("D:\\UTP 2023\\Taller de Programacion\\fichero1.txt");
+            BufferedReader buffer=new BufferedReader(fichero);
+            String linea;
+            while((linea=buffer.readLine())!=null){
+                String[] palabras = linea.split(" ");
+                for(String palabra:palabras){
+                    System.out.println(palabra);
+                }
+            }
+            buffer.close();
+        }catch(IOException io){
+            System.out.println(io.getMessage());
+        }
+
+    }
+}
